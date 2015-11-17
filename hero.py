d39b027e3631b34ee03eb8f1184a86a2d23da42f
student <you@example.com>
2015-11-17 10:18:44 +0300

    pssss

--- hero.py
+++ hero.py
@@ -1,7 +1,16 @@
 # coding: utf-8
 # license: GPLv3
 from gameunit import *
-
+class Hero(Attacker):
+    def __init__(self,name):
+        self._health=100
+        self._attack=50
+        self._experience=0
+    def attack(self,target):
+        target._health -= self._attack
+    def gameOver(self):
+        return self._health <= 0 
+    
 #FIXME:
 """В этом файле должен быть описан класс героя, унаследованный от Attacker
 Герой должен иметь 100 поинтов здоровья, атаку 50, опыт 0, имя, задаваемое в конструкторе
